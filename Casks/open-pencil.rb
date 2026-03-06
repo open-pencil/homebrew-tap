f1e243312553e5adf21372202308e6a9a5b6c8f8
Danila Poyarkov <dev@dannote.net>
2026-03-06 15:43:13 +0300

    Add OpenPencil cask v0.7.0

---
+++ Casks/open-pencil.rb
@@ -0,0 +1,33 @@
+cask "open-pencil" do
+  version "0.7.0"
+
+  on_arm do
+    sha256 "265444e009b6068c3947ea644b3e5a841ea4894a6d9cf31e00d0772c9d887b5e"
+    url "https://github.com/open-pencil/open-pencil/releases/download/v#{version}/OpenPencil_aarch64.app.tar.gz"
+  end
+
+  on_intel do
+    sha256 "885c14cd301593fa321099d76e56bbf62aa658a3183a8844f33a2cf478c69c80"
+    url "https://github.com/open-pencil/open-pencil/releases/download/v#{version}/OpenPencil_x64.app.tar.gz"
+  end
+
+  name "OpenPencil"
+  desc "Open-source design editor compatible with Figma"
+  homepage "https://openpencil.dev"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+
+  app "OpenPencil.app"
+
+  zap trash: [
+    "~/Library/Application Support/net.dannote.open-pencil",
+    "~/Library/Caches/net.dannote.open-pencil",
+    "~/Library/Preferences/net.dannote.open-pencil.plist",
+    "~/Library/Saved Application State/net.dannote.open-pencil.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Tap for OpenPencil
+
+## Install
+
+```sh
+brew install open-pencil/tap/open-pencil
+```
+
+## Update
+
+```sh
+brew upgrade open-pencil
+```
+
+## Uninstall
+
+```sh
+brew uninstall open-pencil
+```
